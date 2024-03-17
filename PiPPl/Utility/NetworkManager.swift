f53134e229b1f65063503c90b95792636e71590d
taek0622 <min4644@gmail.com>
2024-03-18 02:04:32 +0900

    Feat:sparkles:: Add NetworkManager
    - 공지사항을 불러올 네트워크 매니저 추가

---
+++ PiPPl/Utility/NetworkManager.swift
@@ -0,0 +1,40 @@
+//
+//  NetworkManager.swift
+//  PiPPl
+//
+//  Created by 김민택 on 3/17/24.
+//
+
+import Foundation
+
+final class NetworkManager {
+
+    static let shared = NetworkManager()
+
+    var notices = [Notice]()
+
+    private init() {}
+
+    func requestData(completion: @escaping ([Notice]) -> Void) {
+        let semaphore = DispatchSemaphore(value: 0)
+        guard let url = URL(string: "https://raw.githubusercontent.com/taek0622/Notice/main/notice.json") else { return }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "GET"
+
+        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            if let error {
+                print(error)
+                return
+            }
+
+            guard let data = data else { return }
+            let responseData = try! JSONDecoder().decode([Notice].self, from: data)
+            completion(responseData)
+
+            semaphore.signal()
+        }
+        .resume()
+
+        semaphore.wait()
+    }
+}
