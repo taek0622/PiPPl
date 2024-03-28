d03541e47726ad7c8a00a97be6395690d15298b3
taek0622 <min4644@gmail.com>
2024-03-29 02:21:49 +0900

    Feat: Add AppText Enum
    - 앱의 텍스트들을 모아놓은 enum 추가

---
+++ PiPPl/Resource/AppText.swift
@@ -0,0 +1,49 @@
+//
+//  AppText.swift
+//  PiPPl
+//
+//  Created by 김민택 on 3/29/24.
+//
+
+enum AppText {
+
+    // MARK: - TabBar or SideBar Text
+
+    static let localVideo = "로컬 비디오"
+    static let networkVideo = "네트워크 비디오"
+    static let appInfo = "앱 정보"
+
+    // MARK: - Local Video View Text
+
+    static let photoGalleryAccessPermissionButtonText = "사진 앨범 접근 권한을 허용해주세요"
+
+    // MARK: - Network Video View Text
+
+    static let searchFieldPlaceholder = "여기에 주소를 입력해주세요."
+
+    // MARK: - App Info View Text
+
+    static let notice = "공지사항"
+    static let developerInfo = "개발자 정보"
+    static let customerService = "고객센터"
+    static let license = "라이센스"
+    static let versionInfo = "버전 정보"
+
+    static let cantSendMailAlertTitle = "메일 기능 사용 불가"
+    static let cantSendMailAlertBody = "앱에서 메일을 보낼 수 없습니다. 기기의 상태를 확인한 후에 다시 이용해주세요.\n지속적으로 오류가 발생하는 경우 meenu170808@gmail.com으로 별도의 메일 발송 부탁드립니다."
+
+    static let mailTitle = "문의 사항"
+    static let name = "성함"
+    static let mail = "연락처(전화번호/이메일)"
+    static let date = "문의 날짜"
+    static let device = "디바이스 종류"
+    static let os = "운영체제 및 버전"
+    static let appVersion = "앱 버전"
+    static let mailBody = "문의 내용"
+    static let mailComment = "문의 내용을 작성해주세요."
+
+    // MARK: - Info Plist
+
+    static let photoGalleryAccessPermissionInfoPlistText = "갤러리에서 비디오를 가져오기 위해 권한이 필요합니다."
+
+}
