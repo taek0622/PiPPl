849c7761e2987d91b50c56e56fbaaf531d2bfd29
taek0622 <min4644@gmail.com>
2024-04-11 05:29:50 +0900

    Feat:sparkles:: Add AppVersionManager
    - 앱 최신 버전 체크를 위한 AppVersionManager 추가

---
+++ PiPPl/Utility/AppVersionManager.swift
@@ -0,0 +1,16 @@
+//
+//  AppVersionManager.swift
+//  PiPPl
+//
+//  Created by 김민택 on 4/11/24.
+//
+
+import Foundation
+
+class AppVersionManager {
+
+    static let shared = AppVersionManager()
+
+    private init() {}
+
+}
