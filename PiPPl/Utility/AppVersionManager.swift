950e8438f1f13dd952f647ec63fd513162433a77
taek0622 <min4644@gmail.com>
2024-04-11 05:30:52 +0900

    Feat: Add getLatestAppStoreVersion
    - 앱의 앱스토어 최신 버전 정보를 얻기 위한 getLatestAppStoreVersion 메소드 추가
    - async를 이용하여 응답을 받은 후 값을 return

--- PiPPl/Utility/AppVersionManager.swift
+++ PiPPl/Utility/AppVersionManager.swift
@@ -13,4 +13,19 @@ class AppVersionManager {
 
     private init() {}
 
+    private func getLatestAppStoreVersion() async throws -> String {
+        guard let filePath = Bundle.main.path(forResource: "Properties", ofType: "plist"),
+              let property = NSDictionary(contentsOfFile: filePath),
+              let iTunesID = property["iTunesID"] as? String,
+              let url = URL(string: "https://itunes.apple.com/lookup?id=\(iTunesID)")
+        else { return "" }
+
+        let (data, response) = try await URLSession.shared.data(from: url)
+        guard let json = try? JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) as? [String: Any],
+              let results = json["results"] as? [[String: Any]],
+              let latestAppStoreVersion = results[0]["version"] as? String
+        else { return "" }
+
+        return latestAppStoreVersion
+    }
 }
