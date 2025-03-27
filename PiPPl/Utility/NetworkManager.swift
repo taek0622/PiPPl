d8986b6325f520c6db6e7abd88043e0420b4f21c
taek0622 <min4644@gmail.com>
2025-03-27 23:55:15 +0900

    Chore: #15 add requestNoticeData
    NetworkManager에 requestNoticeData 펑션 추가

--- PiPPl/Utility/NetworkManager.swift
+++ PiPPl/Utility/NetworkManager.swift
@@ -37,4 +37,22 @@ final class NetworkManager {
 
         semaphore.wait()
     }
+
+    func requestNoticeData(completion: @escaping ([Notice]) -> Void) {
+        guard let url = URL(string: "https://raw.githubusercontent.com/taek0622/Notice/main/notice.json") else { return }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "GET"
+
+        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            if let error {
+                print(error)
+                return
+            }
+
+            guard let data = data else { return }
+            let responseData = try! JSONDecoder().decode([Notice].self, from: data)
+            completion(responseData)
+        }
+        .resume()
+    }
 }
