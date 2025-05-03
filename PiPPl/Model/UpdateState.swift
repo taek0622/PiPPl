61eec83340acbc94e637ea4a8c0993573bb9f5fa
taek0622 <min4644@gmail.com>
2025-05-04 03:55:22 +0900

    Feat: #46 Add UpdateState model
    - 업데이트 알림을 관리하기 위한 UpdateState 모델 추가

---
+++ PiPPl/Model/UpdateState.swift
@@ -0,0 +1,15 @@
+//
+//  UpdateState.swift
+//  PiPPl
+//
+//  Created by 김민택 on 5/3/25.
+//
+
+import SwiftUI
+
+enum UpdateState {
+    case latest
+    case available
+    case recommended
+    case required
+}
