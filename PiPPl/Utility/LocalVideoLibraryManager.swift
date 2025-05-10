5739c93e573ceaa95b9b23360e2fb386ed0bab7a
taek0622 <min4644@gmail.com>
2025-05-11 02:05:02 +0900

    Feat: #52 Add ThumbnailDiskCache
    - 썸네일을 위한 디스크 캐시를 사용하기 위한 싱글톤 클래스 추가

--- PiPPl/Utility/LocalVideoLibraryManager.swift
+++ PiPPl/Utility/LocalVideoLibraryManager.swift
@@ -37,6 +37,53 @@ class ThumbnailMemoryCache {
 
 }
 
+class ThumbnailDiskCache {
+
+    static let shared = ThumbnailDiskCache()
+
+    private init() {}
+
+    private let fileManager = FileManager.default
+
+    private var cacheDirectoryURL: URL {
+        let caches = fileManager.urls(for: .cachesDirectory, in: .userDomainMask).first!
+        return caches.appending(path: "ThumbnailCache", directoryHint: .isDirectory)
+    }
+
+    func saveThumbnail(_ image:UIImage, for asset: PHAsset) {
+        let url = fileURL(for: asset)
+
+        if !fileManager.fileExists(atPath: cacheDirectoryURL.path) {
+            try? fileManager.createDirectory(at: cacheDirectoryURL, withIntermediateDirectories: true)
+        }
+
+        if let data = image.jpegData(compressionQuality: 0.8) {
+            try? data.write(to: url)
+        }
+    }
+
+    func loadThumbnail(for asset: PHAsset) -> UIImage? {
+        let url = fileURL(for: asset)
+        guard fileManager.fileExists(atPath: url.path),
+              let data = try? Data(contentsOf: url),
+              let image = UIImage(data: data)
+        else { return nil }
+
+        return image
+    }
+
+    func removeThumbnail(for asset: PHAsset) {
+        let url = fileURL(for: asset)
+        try? fileManager.removeItem(at: url)
+    }
+
+    private func fileURL(for asset: PHAsset) -> URL {
+        let filename = asset.localIdentifier.replacingOccurrences(of: "/", with: "_")
+        return cacheDirectoryURL.appendingPathComponent("\(filename)", conformingTo: .jpeg)
+    }
+
+}
+
 class LocalVideoLibraryManager: NSObject, ObservableObject, PHPhotoLibraryChangeObserver {
 
     @Published var videos = [Video]()
