00a4571a82fe5a84323d79ce26792b94bab34949
taek0622 <min4644@gmail.com>
2025-06-01 22:03:07 +0900

    Remove: Delete unused code
    - 더 이상 사용되지 않는 기존 네비게이션 pip 버튼 및 관련 기능 삭제

--- PiPPl/View/NetworkVideo/NetworkPlayerView.swift
+++ PiPPl/View/NetworkVideo/NetworkPlayerView.swift
@@ -11,11 +11,10 @@ struct NetworkPlayerView: View {
     @Environment(\.colorScheme) var colorScheme
     @State private var searchingText = "https://www.google.com/"
     @State private var isSubmitted = false
-    @State private var isPiPOn = false
     @State private var isPausedVideo = false
 
     var body: some View {
-        WebView(searchingText: $searchingText, isSubmitted: $isSubmitted, isPiPOn: $isPiPOn, isPausedVideo: $isPausedVideo)
+        WebView(searchingText: $searchingText, isSubmitted: $isSubmitted, isPausedVideo: $isPausedVideo)
             .toolbar {
                 ToolbarItem(placement: .principal) {
                     TextField(AppText.searchFieldPlaceholder, text: $searchingText)
@@ -25,14 +24,6 @@ struct NetworkPlayerView: View {
                         .onSubmit {
                             isSubmitted = true
                         }
-                }
-
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button {
-                        isPiPOn = true
-                    } label: {
-                        Image(systemName: "pip.enter")
-                    }
                 }
             }
             .onDisappear {
